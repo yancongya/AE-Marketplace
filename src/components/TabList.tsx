65fb81850fa56b6058aeb56671a0db8b3570ecaa
tycon <Cubetan57@gmail.com>
2026-03-19 10:25:08 +0800

    fix: 统一首页和各页面的卡片网格布局样式

--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -146,7 +146,7 @@ export function TabList<T extends ContentItem | PresetItem | ExtensionItem>({
       searchValue={searchTerm}
       onSearchChange={setSearchTerm}
     >
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {paginatedList.map((item) => (
           <TabCard
             key={item.slug}
